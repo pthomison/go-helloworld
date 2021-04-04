222703d428a12f9b108c7e292666246fe50cb1dd
p.thomison <p.thomison@gmail.com>
2021-04-04 13:30:45 -0700

    adding cobra

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
-	// "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World")
+  if err := rootCmd.Execute(); err != nil {
+    fmt.Fprintln(os.Stderr, err)
+    os.Exit(1)
+  }
+}
+
+var rootCmd = &cobra.Command{
+  Use:   "go-helloworld",
+  Short: "go-helloworld",
+  Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("hello world")
+  },
 }
