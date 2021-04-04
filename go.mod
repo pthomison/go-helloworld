452f09a212f5a74045bb9312f1c192144d63f201
p.thomison <p.thomison@gmail.com>
2021-04-04 13:33:07 -0700

    cleaning

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/pthomison/go-helloworld
 
 go 1.15
 
-require github.com/spf13/cobra v1.1.3 // indirect
+require github.com/spf13/cobra v1.1.3
--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"github.com/spf13/cobra"
 	"fmt"
+	"github.com/spf13/cobra"
 	"os"
 )
 
 func main() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
 
 var rootCmd = &cobra.Command{
-  Use:   "go-helloworld",
-  Short: "go-helloworld",
-  Run: func(cmd *cobra.Command, args []string) {
-    fmt.Println("hello world")
-  },
+	Use:   "go-helloworld",
+	Short: "go-helloworld",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("hello world")
+	},
 }
